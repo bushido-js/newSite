b96c58a59ed9dabeb3aeb38a5a7661ddc9ad23b2
Oleg Bidikhov <olbid99@gmail.com>
2022-06-07 22:10:50 +0300

    base vresion 1.1

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Все описанное ниже тест, в дальнейшем автоматизирую полностью заполнение портфолио 
+
+// let field = document.querySelector('#test');
+// // Создаём поле gallery(Даёт grid)
+//   let gallery = document.createElement('div');
+//   gallery.classList.add('gallery');
+//   field.appendChild(gallery);
+
+// //Создаем окна, gallery-window
+//   let galleryWindow = document.createElement('div');
+//   galleryWindow.classList.add('gallery-window');
+//   gallery.appendChild(galleryWindow);
+
+// //Создаем окно для изображения
+//   let imageWrapper = document.createElement('div');
+//   imageWrapper.classList.add('image-wrapper');
+//   galleryWindow.appendChild(imageWrapper);
+
+// console.log(galleryWindow);
+
+// // function addJob ()
+
